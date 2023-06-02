a6a7f04cc3d94fd4c2d8428728f41fff9ab3c90d
Lucas Gustavo Vicente Carli <126676462+Lucasvcarli@users.noreply.github.com>
2023-06-02 16:13:08 -0300

    commiti

--- js/script.js
+++ js/script.js
@@ -20,3 +20,51 @@ let food ={
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box
 }
+
+// Função para criar o Background
+function criarBG() {
+    context.fillStyle = "lightgreen";
+    // Desenha o retângulo usando x e y e a largura e altura setadas
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+// Função para criar a cobrinha
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+// Quando um evento acontece, detecta e chama a função update
+document.addEventListener('Keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction !='right') direction = 'left';
+    if(event.keyCode == 38 && direction !='down') direction = 'up';
+    if(event.keyCode == 39 && direction !='left') direction = 'right';
+    if(event.keyCode == 40 && direction !='up') direction = 'down';
+}
+
+// Função Principal
+function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == "right"){
+        snake[0].x = 0;
+    }
+    if(snake[0].x < 0*box && direction == "left"){
+        snake[0].x = 16;
+    }
+    if(snake[0].y > 15*box && direction == "down"){
+        snake[0].y = 0;
+    }
+    if(snake[0].y < 0*box && direction == "up"){
+        snake[0].y = 16;
+    }
+
+    criarBG();
+    criarCobrinha();
+    drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+}
